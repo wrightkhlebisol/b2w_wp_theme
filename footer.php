d3ef6d30bbf95a507ce3371977cf078c9a42c39c
Wright <wrightkhlebisol@gmail.com>
2020-02-23 08:33:32 +0100

    converted static page to wordpress home template

---
+++ footer.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BootstrapToWordpress
+ */
+
+?>
+
+</div><!-- #content -->
+
+<!-- FOOTER
+	================================================== -->
+<footer>
+	<div class="container">
+		<div class="col-sm-3">
+			<p><a href="/"><img src="<?php bloginfo('stylesheet_directory') ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a></p>
+		</div><!-- end col -->
+		<div class="col-sm-6">
+			<nav>
+				<ul class="list-unstyled list-inline">
+					<li><a href="">Home</a></li>
+					<li><a href="">Blog</a></li>
+					<li><a href="">Resources</a></li>
+					<li><a href="">Contact</a></li>
+					<li class="signup-link"><a href="">Sign up now</a></li>
+				</ul>
+			</nav>
+		</div><!-- end col -->
+		<div class="col-sm-3">
+			<p class="pull-right">&copy; 2014 Wright</p>
+		</div><!-- end col -->
+	</div><!-- container -->
+</footer>
+
+
+<!-- MODAL
+	================================================== -->
+<div class="modal fade" id="myModal">
+	<div class="modal-dialog">
+		<div class="modal-content">
+
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our Mailing List</h4>
+			</div><!-- modal-header -->
+
+			<div class="modal-body">
+				<p>Simply enter your name and email! As a thank you for joining us, we're going to give you one of our best-selling courses, <em>for free!</em></p>
+
+				<form class="form-inline" role="form">
+					<div class="form-group">
+						<label class="sr-only" for="subscribe-name">Your first name</label>
+						<input type="text" class="form-control" id="subscribe-name" placeholder="Your first name">
+					</div>
+					<div class="form-group">
+						<label class="sr-only" for="subscribe-email">and your email</label>
+						<input type="text" class="form-control" id="subscribe-email" placeholder="and your email">
+					</div>
+					<input type="submit" class="btn btn-danger" value="Subscribe!">
+				</form>
+
+				<hr>
+
+				<p><small>By providing your email you consent to receiving occasional promotional emails &amp; newsletters. <br>No Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+			</div><!-- modal-body -->
+
+		</div><!-- modal-content -->
+	</div><!-- modal-dialog -->
+</div><!-- modal -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/jquery-2.1.1.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/bootstrap.min.js"></script>
+
+<script src="<?php bloginfo('template_directory') ?>/assets/js/main.js"></script>
+
+<script type="text/javascript" src="//use.typekit.net/gla7wnd.js"></script>
+<script type="text/javascript">
+	try {
+		Typekit.load();
+	} catch (e) {}
+</script>
+
+</body>
+
+</html>
