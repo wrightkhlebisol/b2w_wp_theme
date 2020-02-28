d5193246736ab2d245416146b12374f150157965
Wright <wrightkhlebisol@gmail.com>
2020-02-28 11:17:19 +0100

    Created homepage template and footer

--- footer.php
+++ footer.php
@@ -12,7 +12,20 @@
 
 ?>
 
-</div><!-- #content -->
+<?php wp_footer(); ?>
+
+<!-- SIGN UP SECTION
+	================================================== -->
+<section id="signup" data-type="background" data-speed="4">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
+				<p><a href="" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
+			</div><!-- end col -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- signup -->
 
 <!-- FOOTER
 	================================================== -->
@@ -73,9 +86,7 @@
 		</div><!-- modal-content -->
 	</div><!-- modal-dialog -->
 </div><!-- modal -->
-</div><!-- #page -->
 
-<?php wp_footer(); ?>
 
 <!-- Bootstrap core JavaScript
     ================================================== -->
--- page-home.php
+++ page-home.php
@@ -353,19 +353,5 @@ get_header();
 	</div><!-- container -->
 </section><!-- kudos -->
 
-
-<!-- SIGN UP SECTION
-	================================================== -->
-<section id="signup" data-type="background" data-speed="4">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-6 col-sm-offset-3">
-				<h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
-				<p><a href="" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
-			</div><!-- end col -->
-		</div><!-- row -->
-	</div><!-- container -->
-</section><!-- signup -->
-
 <?php
 get_footer();
