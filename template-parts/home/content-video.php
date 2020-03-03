9bc922d642ecc2af1bd256ae3daab2ff22c578c4
Wright <wrightkhlebisol@gmail.com>
2020-03-03 09:01:36 +0100

    Fixed broken embed link

--- template-parts/home/content-video.php
+++ template-parts/home/content-video.php
@@ -1,6 +1,6 @@
 <?php
 $youtube_section_title = get_field('youtube_section_title');
-$youtube_section_object = get_field('youtube_section_object');
+$youtube_section_url = get_field('youtube_section_url');
 ?>
 <!-- VIDEO FEATURETTE
 	================================================== -->
@@ -9,7 +9,7 @@ $youtube_section_object = get_field('youtube_section_object');
         <div class="row">
             <div class="col-sm-8 col-sm-offset-2">
                 <h2><?php echo $youtube_section_title ?></h2>
-                <?php echo $youtube_section_object ?>
+                <iframe width="100%" height="415" src="<?php echo $youtube_section_url ?>" frameborder="0" allowfullscreen></iframe>
             </div><!-- end col -->
         </div><!-- row -->
     </div><!-- container -->
