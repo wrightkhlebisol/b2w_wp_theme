0613a170fd6a0d75a47955c769d9fa08c1c08209
Wright <wrightkhlebisol@gmail.com>
2020-03-03 18:47:23 +0100

    converted instructor section to dynamic part

--- template-parts/home/content-instructor.php
+++ template-parts/home/content-instructor.php
@@ -1,57 +1,61 @@
-<!-- INSTRUCTOR
-	================================================== -->
+<?php
+$instructor_section_title = get_field('instructor_section_title');
+$instructor_section_author_name = get_field('instructor_section_author_name');
+$instructor_section_twitter = get_field('instructor_section_twitter');
+$instructor_section_facebook = get_field('instructor_section_facebook');
+$instructor_section_google = get_field('instructor_section_google');
+$instructor_section_body = get_field('instructor_section_body');
+$instructor_section_numbers_title = get_field('instructor_section_numbers_title');
+$instructor_section_numbers_students = get_field('instructor_section_numbers_students');
+$instructor_section_numbers_reviews = get_field('instructor_section_numbers_reviews');
+$instructor_section_numbers_courses = get_field('instructor_section_numbers_courses');
+?>
+
+<!--============== INSTRUCTOR ================== -->
 <section id="instructor">
     <div class="container">
         <div class="row">
             <div class="col-sm-8 col-md-6">
                 <div class="row">
                     <div class="col-lg-8">
-                        <h2>Your Instructor <small>Brad Hussey</small></h2>
+                        <h2><?php echo $instructor_section_title ?> <small><?php echo $instructor_section_author_name ?></small></h2>
                     </div><!-- end col -->
                     <div class="col-lg-4">
-                        <a href="https://twitter.com/bradhussey" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-                        <a href="https://facebook.com/bradhussey" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-                        <a href="https://plus.google.com/+BradHussey" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
+                        <a href="https://twitter.com/<?php echo $instructor_section_twitter ?>" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+                        <a href="https://facebook.com/<?php echo $instructor_section_facebook ?>" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <a href="<?php echo $instructor_section_google ?>https://plus.google.com/+BradHussey" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
                     </div><!-- end col -->
 
                 </div><!-- row -->
 
-                <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and digital entrepreneur.<p>
+                <?php echo $instructor_section_body ?>
 
-                        <p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on the web, crafting design solutions, and speaking in code.</p>
+                <h3><?php echo $instructor_section_numbers_title ?> <small>They Don't Lie</small></h3>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="num">
+                            <div class="num-content">
+                                <?php echo $instructor_section_numbers_students ?> <span>students</span>
+                            </div><!-- num-content -->
+                        </div><!-- num -->
+                    </div><!-- end col -->
 
-                        <p>Brad's determination and love for what he does has landed him in some pretty interesting places with some neat people. He's had the privilege of working with, and providing solutions for, numerous businesses, big &amp; small, across the Americas.</p>
+                    <div class="col-xs-4">
+                        <div class="num">
+                            <div class="num-content">
+                                <?php echo $instructor_section_numbers_reviews ?> <span>reviews</span>
+                            </div><!-- num-content -->
+                        </div><!-- num -->
+                    </div><!-- end col -->
 
-                        <p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality web design tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
-
-                        <hr>
-
-                        <h3>The Numbers <small>They Don't Lie</small></h3>
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <div class="num">
-                                    <div class="num-content">
-                                        41,000+ <span>students</span>
-                                    </div><!-- num-content -->
-                                </div><!-- num -->
-                            </div><!-- end col -->
-
-                            <div class="col-xs-4">
-                                <div class="num">
-                                    <div class="num-content">
-                                        568 <span>reviews</span>
-                                    </div><!-- num-content -->
-                                </div><!-- num -->
-                            </div><!-- end col -->
-
-                            <div class="col-xs-4">
-                                <div class="num">
-                                    <div class="num-content">
-                                        8 <span>courses</span>
-                                    </div><!-- num-content -->
-                                </div><!-- num -->
-                            </div><!-- end col -->
-                        </div><!-- row -->
+                    <div class="col-xs-4">
+                        <div class="num">
+                            <div class="num-content">
+                                <?php echo $instructor_section_numbers_courses ?> <span>courses</span>
+                            </div><!-- num-content -->
+                        </div><!-- num -->
+                    </div><!-- end col -->
+                </div><!-- row -->
 
             </div><!-- end col -->
         </div><!-- row -->
