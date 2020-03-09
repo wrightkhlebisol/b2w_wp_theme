44f12c262899bdd5c69d58be2605dc416328ea81
Wright <wrightkhlebisol@gmail.com>
2020-03-09 18:58:00 +0100

    Edited main post content area to suit theme

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -10,27 +11,32 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<!-- MAIN CONTENT
+	================================================== -->
+<div class="container">
+	<div class="row" id="primary">
+		<main id="content" class="col-sm-8">
+			<?php
+			while (have_posts()) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part('template-parts/content', get_post_type());
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				the_post_navigation();
 
-			the_post_navigation();
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			?>
+		</main>
+	</div>
+</div>
 
-		endwhile; // End of the loop.
-		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+
 
 <?php
 get_sidebar();
