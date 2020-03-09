04783544843dd288daa80a73cf91cf195393e313
Wright <wrightkhlebisol@gmail.com>
2020-03-09 12:19:25 +0100

    converted default index page to custom post page

--- page-blog.php
+++ page-blog.php
@@ -6,150 +6,147 @@
 
 get_header();
 
-<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
-		<h1 class="page-title">Blog</h1>
-	</section>
-    
-    <!-- BLOG CONTENT
-	================================================== -->
-    <div class="container">
-	    <div class="row" id="primary">
-	    
-		    <main id="content" class="col-sm-8" role="main">
-		    	
-		    	<article class="post">
-		    		<header>
-		    			<h3><a href="post.html">Blog title here</a></h3>
-		    			<div class="post-details">
-			    			<i class="fa fa-user"></i> Brad Hussey
-							<i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
-							<i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
-							<i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
-							
-							<div class="post-comments-badge">
-								<a href=""><i class="fa fa-comments"></i> 168</a>
-							</div><!-- post-comments-badge -->
-		    			</div><!-- post-details -->
-		    		</header>
-		    		<div class="post-image">
-		    			<img src="assets/img/hero-bg.jpg">
-		    		</div><!-- post-image -->
-		    		<div class="post-excerpt">
-		    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
-		    		</div><!-- post-excerpt -->
-		    	</article><!-- post -->
-		    	
-		    	<article class="post">
-		    		<header>
-		    			<h3><a href="">A different blog post, with a longer title</a></h3>
-		    			<div class="post-details">
-			    			<i class="fa fa-user"></i> Brad Hussey
-							<i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
-							<i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
-							<i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
-							
-							<div class="post-comments-badge">
-								<a href=""><i class="fa fa-comments"></i> 168</a>
-							</div><!-- post-comments-badge -->
-		    			</div><!-- post-details -->
-		    		</header>
-		    		<div class="post-image">
-		    			<img src="assets/img/hero-bg.jpg">
-		    		</div><!-- post-image -->
-		    		<div class="post-excerpt">
-		    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
-		    		</div><!-- post-excerpt -->
-		    	</article><!-- post -->
-		    	
-		    	<article class="post">
-		    		<header>
-		    			<h3><a href="">Short n' sweet</a></h3>
-		    			<div class="post-details">
-			    			<i class="fa fa-user"></i> Brad Hussey
-							<i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
-							<i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
-							<i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
-							
-							<div class="post-comments-badge">
-								<a href=""><i class="fa fa-comments"></i> 168</a>
-							</div><!-- post-comments-badge -->
-		    			</div><!-- post-details -->
-		    		</header>
-		    		<div class="post-image">
-		    			<img src="assets/img/hero-bg.jpg">
-		    		</div><!-- post-image -->
-		    		<div class="post-excerpt">
-		    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
-		    		</div><!-- post-excerpt -->
-		    	</article><!-- post -->
-		    	
-		    </main><!-- content -->
-		    
-		    
-		    <!-- SIDEBAR
-			================================================== -->
-		    <aside class="col-sm-4">
-		    	
-		    	<div class="widget">
-		    		<h4>Join our Mailing List</h4>
-		    		<p>Keep up-to-date with the latest news, and we'll <strong>send you something special as a thank you!</strong></p>
-		    		<button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
-						Click here to subscribe
-					</button>
-		    	</div><!-- widget -->
-		    	
-		    	<div class="widget">
-		    		<form role="form" class="search-form">
-		    			<label for="sidebar-search" class="sr-only">Search the blog</label>
-		    			<input type="text" placeholder="Search the blog..." id="sidebar-search">
-		    		</form>
-		    	</div><!-- widget -->
-		    	
-		    	<div class="widget">
-		    		<h4>About Bootstrap to Wordpress</h4>
-		    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-		    	</div><!-- widget -->
-		    	
-		    	<div class="widget">
-		    		<h4>Recent Posts</h4>
-		    		<ul>
-		    			<li><a href="">Blog post #1</a></li>
-		    			<li><a href="">Blog post #2</a></li>
-		    			<li><a href="">Blog post #3</a></li>
-		    			<li><a href="">Blog post #4</a></li>
-		    			<li><a href="">Blog post #5</a></li>
-		    		</ul>
-		    	</div><!-- widget -->
-		    	
-		    	<div class="widget">
-		    		<h4>Categories</h4>
-		    		<ul>
-		    			<li><a href="">Category #1</a></li>
-		    			<li><a href="">Category #2</a></li>
-		    			<li><a href="">Category #3</a></li>
-		    			<li><a href="">Category #4</a></li>
-		    			<li><a href="">Category #5</a></li>
-		    			<li><a href="">Category #6</a></li>
-		    		</ul>
-		    	</div><!-- widget -->
-		    	
-		    </aside>
-	    
-	    </div><!-- primary -->
-    </div><!-- container -->
-	
-	<!-- SIGN UP SECTION
-	================================================== -->
-	<section id="signup" data-type="background" data-speed="5">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-6 col-sm-offset-3">
-					<h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
-					<p><a href="" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
-				</div><!-- end col -->
-			</div><!-- row -->
-		</div><!-- container -->
-	</section><!-- signup -->
+?>
 
+<?php if (has_post_thumbnail()) : ?>
+    <section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">Blog</h1>
+    </section>
+<?php else : ?>
+    <section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">Blog</h1>
+    </section>
+<?php endif; ?>
+
+<!-- BLOG CONTENT
+	================================================== -->
+<div class="container">
+    <div class="row" id="primary">
+
+        <main id="content" class="col-sm-8" role="main">
+
+            <article class="post">
+                <header>
+                    <h3><a href="post.html">Blog title here</a></h3>
+                    <div class="post-details">
+                        <i class="fa fa-user"></i> Brad Hussey
+                        <i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
+                        <i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
+                        <i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
+
+                        <div class="post-comments-badge">
+                            <a href=""><i class="fa fa-comments"></i> 168</a>
+                        </div><!-- post-comments-badge -->
+                    </div><!-- post-details -->
+                </header>
+                <div class="post-image">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/hero-bg.jpg">
+                </div><!-- post-image -->
+                <div class="post-excerpt">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
+                </div><!-- post-excerpt -->
+            </article><!-- post -->
+
+            <article class="post">
+                <header>
+                    <h3><a href="">A different blog post, with a longer title</a></h3>
+                    <div class="post-details">
+                        <i class="fa fa-user"></i> Brad Hussey
+                        <i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
+                        <i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
+                        <i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
+
+                        <div class="post-comments-badge">
+                            <a href=""><i class="fa fa-comments"></i> 168</a>
+                        </div><!-- post-comments-badge -->
+                    </div><!-- post-details -->
+                </header>
+                <div class="post-image">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/hero-bg.jpg">
+                </div><!-- post-image -->
+                <div class="post-excerpt">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
+                </div><!-- post-excerpt -->
+            </article><!-- post -->
+
+            <article class="post">
+                <header>
+                    <h3><a href="">Short n' sweet</a></h3>
+                    <div class="post-details">
+                        <i class="fa fa-user"></i> Brad Hussey
+                        <i class="fa fa-clock-o"></i> <time>August 7, 2014</time>
+                        <i class="fa fa-folder"></i> <a href="">Tutorials</a>, <a href="">Coding</a>
+                        <i class="fa fa-tags"></i> Tagged <a href="">wordpress</a>, <a href="">premium</a>, <a href="">another tag</a>, <a href="">yadda yadaa</a>
+
+                        <div class="post-comments-badge">
+                            <a href=""><i class="fa fa-comments"></i> 168</a>
+                        </div><!-- post-comments-badge -->
+                    </div><!-- post-details -->
+                </header>
+                <div class="post-image">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/hero-bg.jpg">
+                </div><!-- post-image -->
+                <div class="post-excerpt">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt... <a href="post.html">continue reading &raquo;</a></p>
+                </div><!-- post-excerpt -->
+            </article><!-- post -->
+
+        </main><!-- content -->
+
+
+        <!-- SIDEBAR
+			================================================== -->
+        <aside class="col-sm-4">
+
+            <div class="widget">
+                <h4>Join our Mailing List</h4>
+                <p>Keep up-to-date with the latest news, and we'll <strong>send you something special as a thank you!</strong></p>
+                <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal">
+                    Click here to subscribe
+                </button>
+            </div><!-- widget -->
+
+            <div class="widget">
+                <form role="form" class="search-form">
+                    <label for="sidebar-search" class="sr-only">Search the blog</label>
+                    <input type="text" placeholder="Search the blog..." id="sidebar-search">
+                </form>
+            </div><!-- widget -->
+
+            <div class="widget">
+                <h4>About Bootstrap to Wordpress</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div><!-- widget -->
+
+            <div class="widget">
+                <h4>Recent Posts</h4>
+                <ul>
+                    <li><a href="">Blog post #1</a></li>
+                    <li><a href="">Blog post #2</a></li>
+                    <li><a href="">Blog post #3</a></li>
+                    <li><a href="">Blog post #4</a></li>
+                    <li><a href="">Blog post #5</a></li>
+                </ul>
+            </div><!-- widget -->
+
+            <div class="widget">
+                <h4>Categories</h4>
+                <ul>
+                    <li><a href="">Category #1</a></li>
+                    <li><a href="">Category #2</a></li>
+                    <li><a href="">Category #3</a></li>
+                    <li><a href="">Category #4</a></li>
+                    <li><a href="">Category #5</a></li>
+                    <li><a href="">Category #6</a></li>
+                </ul>
+            </div><!-- widget -->
+
+        </aside>
+
+    </div><!-- primary -->
+</div><!-- container -->
+
+<?php
 
 get_footer();
+?>
